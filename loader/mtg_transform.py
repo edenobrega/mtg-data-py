4f6a1503eea6184d3a10ba06b8ab77ad6c9b6d4c
nobs <edenobrega@sky.com>
2024-04-27 23:04:58 +0100

    move transform funcs to its own module

--- loader/mtg_transform.py
+++ loader/mtg_transform.py
@@ -1,10 +1,8 @@
 import pandas as pd
 import numpy as np
 import logging
-from collections import namedtuple
 log = logging.getLogger("__main__")
 
-# Some cards with two faces only have one image, and those images will be in the usual
 def get_card_faces(cards: pd.DataFrame):
     log.info("preparing card faces")
     # Cards with actual multiple faces
@@ -74,7 +72,6 @@ def get_card_faces(cards: pd.DataFrame):
     log.info("finished preparing card faces")
     return card_faces
 
-# Extract certain columns
 def get_card_parts(cards: pd.DataFrame):
     log.info("preparing card parts")
     column_check = ["card_id", "object", "component", "related_card"]
@@ -103,22 +100,41 @@ def get_type_line_data(cards: pd.DataFrame):
     card_types_lookup_root = card_typeline_lookup.drop_duplicates().reset_index().drop(["index"],axis=1) 
 
     # some cards dont have a type_line in its root, and instead its nested in its faces
-    card_no_typeline = cards.loc[cards["type_line"].isna(), ["id","card_faces"]].copy()
-    face_explode = card_no_typeline.explode("card_faces")
-    nested_faces = pd.json_normalize(face_explode["card_faces"]).set_index(face_explode.index)
-    nested_faces = nested_faces.loc[:, ["type_line"]]
-    type_line_with_id = nested_faces["type_line"].str.split(" ").explode()
-    card_to_type_premap_nested = pd.merge(card_no_typeline["id"], type_line_with_id, left_index=True, right_index=True)
-    card_types_lookup_nested = type_line_with_id.drop_duplicates().reset_index().drop(["index"],axis=1) 
-
-    card_types_lookup = pd.concat([card_types_lookup_root, card_types_lookup_nested])
-
-    card_to_type_premap = pd.concat([card_to_type_premap_root, card_to_type_premap_nested])
+    if "card_faces" in cards:
+        card_no_typeline = cards.loc[cards["type_line"].isna(), ["id","card_faces"]].copy()
+        face_explode = card_no_typeline.explode("card_faces")
+        nested_faces = pd.json_normalize(face_explode["card_faces"]).set_index(face_explode.index)
+        nested_faces = nested_faces.loc[:, ["type_line"]]
+        type_line_with_id = nested_faces["type_line"].str.split(" ").explode()
+        card_to_type_premap_nested = pd.merge(card_no_typeline["id"], type_line_with_id, left_index=True, right_index=True)
+        card_types_lookup_nested = type_line_with_id.drop_duplicates().reset_index().drop(["index"],axis=1) 
+        card_types_lookup = pd.concat([card_types_lookup_root, card_types_lookup_nested])
+        card_to_type_premap = pd.concat([card_to_type_premap_root, card_to_type_premap_nested])
+    else:
+        card_to_type_premap = card_to_type_premap
     card_to_type_premap = card_to_type_premap.reset_index()
     card_to_type_premap.loc[card_to_type_premap["type_name"].isna(), "type_name"] = card_to_type_premap["type_line"] 
     card_to_type_premap.drop(["type_line"], axis=1, inplace=True)
     card_to_type_premap.set_index(["index"], inplace=True)
 
-    type_line_tuple = namedtuple("type_line_tuple", "premap lookup")
     log.info("finished preparing type line data")
-    return type_line_tuple(card_to_type_premap, card_types_lookup)
+    return card_types_lookup, card_to_type_premap
+
+def get_rarities(cards: pd.DataFrame):
+    return cards["rarity"].drop_duplicates()
+
+def get_layouts(cards: pd.DataFrame):
+    return cards["layout"].drop_duplicates()
+
+def get_cards(_cards: pd.DataFrame):
+    if "card_faces" not in _cards:
+        _cards["card_faces"] = pd.NA
+    cards = _cards.loc[:, ["name", "mana_cost", "oracle_text", "flavor_text", "artist", "collector_number",
+                              "power", "toughness", "set", "id", "cmc", "oracle_id", "rarity", "layout", "card_faces", "image_uris"]]
+
+    cards_no_multi: pd.DataFrame = cards.loc[cards["card_faces"].isna(), ["id", "image_uris"]]
+    if cards_no_multi.empty == False:
+        images = pd.json_normalize(cards_no_multi["image_uris"]).set_index(cards_no_multi.index)
+        cards = pd.merge(cards, images["normal"], left_index=True, right_index=True, how="left")
+    cards = cards.drop(["image_uris", "card_faces"], axis=1)
+    return cards
